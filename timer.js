ae9da978bbb87893b682777e632e76daf3b77932
Joseph Njoroge <139031556+Joseph-Njoro@users.noreply.github.com>
2024-05-14 03:44:01 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,6 +1,7 @@
 const countdown = (seconds) => {
     console.log("Starting countdown...");
 
+    
     const interval = setInterval(() => {
         if (seconds > 0) {
             console.log(`${seconds} seconds remaining...`);
@@ -15,6 +16,7 @@ const countdown = (seconds) => {
 const printMotivationalMessage = () => {
     console.log("Congratulations! You've completed the countdown. Time to seize the day and make things happen!");
 };
+
 
 // Set the countdown duration in seconds
 const countdownDuration = 5;
